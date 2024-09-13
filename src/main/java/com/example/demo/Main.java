5d6f1f138100f588e63d17bea7e58b5962d065fa
Nkwelle Ntungwe <nnnkwell@lakeheadu.ca>
2024-09-13 12:40:15 -0400

    m

---
+++ src/main/java/com/example/demo/Main.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class Main{
+    public static void main(String[] args) {
+        Sport sport = new Sport("backreetball", 6.9, "sondahr isa");
+        sport.setSports("BasketBall");
+        sport.setHeight(6.3);
+        sport.setPhysical_activity("Run and try to get the basketball in the Net");
+        System.out.println(sport);
+
+    }
+}
---
+++ src/main/java/com/example/demo/Sport.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+public class Sport {
+    private String sports;
+    private double height;
+    private  String physical_activity;
+
+    public Sport(String sports, double height, String physical_activity){
+        setSports(sports);
+        setHeight(height);
+        setPhysical_activity(physical_activity);
+    }
+
+    public String getSports(){
+        return sports;
+    }
+
+    public void setSports(String sports){
+        this.sports= sports;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        if(height < 6.2)
+            throw new IllegalArgumentException("You are too short to play any height based sports. Give up!!!");
+        else if(height > 9)
+            throw  new IllegalArgumentException("You did not clearly read the instructions, give a realistic Height");
+
+        else
+            this.height = height;
+    }
+
+    public String getPhysical_activity() {
+        return physical_activity;
+    }
+
+
+    public  String toString(){
+        return  "The sport you play is:" + sports +'\n'+
+                "your height is: "+ height +'\n'+ physical_activity;
+    }
+
+
+    public void setPhysical_activity(String physical_activity) {
+        this.physical_activity = physical_activity;
+    }
+}
